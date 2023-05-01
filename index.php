09cb5a6e7b1d3ab542807cfc39876e26e8336275
吳亮均 <s129439@yahoo.com.tw>
2023-05-01 21:53:32 +0800

    homepush0501

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 <form action="target.php" method="get">
     <input type="text" name="addr" id="name">
     <!-- <input type="text" name="name" id="name"> -->
-    <!-- 年齡:<input type="text" name="age" id="name"> -->
+    年齡:<input type="text" name="age" id="name">
     <input type="submit" value="送出">
 </form>
 
 <h3>POST</h3>
 <form action="target.php" method="post">
-<input type="text" name="name" id="name">
-<input type="submit" value="送出">
+    <input type="text" name="name" id="name">
+    <input type="submit" value="送出">
 </form>
 <!-- 一個表單只能用一種方式送，不能同時get又post -->
 <h3>POST及檔案上傳</h3>
 <!-- multipart多媒體/form-data表單資料 -->
 <form action="target.php" method="post" enctype="multipart/form-data">
-<input type="text" name='type'>
-<input type="file" name="img" id="">
-<input type="submit" value="送出">
+    <input type="text" name='type'>
+    <input type="file" name="img" id="">
+    <input type="submit" value="送出">
 </form>
--- target.php
+++ target.php
@@ -34,30 +34,30 @@ if(empty($a)){
 //     echo "假";
 // }
 
-// if (!empty($_GET)) {
-//     echo "以下資料為get表單的資料<br>";
-//     $age=$_GET['age'];
-//     echo "您的年紀為".$age."<br>";
-//     if($age>=45){
-//         echo "屬於中高齡";
-//     }else if($age>=35){
-//         echo "屬於中年人";
-//     }else if($age>=25){
-//         echo "屬於青年";
-//     }else{
-//         echo "屬於少年";
-//     }
-    // echo "<pre>";
-    // print_r($_GET);
-    // echo "</pre>";
-// }
+if (!empty($_GET)) {
+    echo "以下資料為get表單的資料<br>";
+    $age=$_GET['age'];
+    echo "您的年紀為".$age."<br>";
+    if($age>=45){
+        echo "屬於中高齡";
+    }else if($age>=35){
+        echo "屬於中年人";
+    }else if($age>=25){
+        echo "屬於青年";
+    }else{
+        echo "屬於少年";
+    }
+    echo "<pre>";
+    print_r($_GET);
+    echo "</pre>";
+}
 
-// if (!empty($_POST)) {
-//     echo "以下資料為post表單的資料<br>";
-//     echo "<pre>";
-//     print_r($_POST);
-//     echo "</pre>";
-// }
+if (!empty($_POST)) {
+    echo "以下資料為post表單的資料<br>";
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+}
 
 // echo $_GET['addr'];
 // echo $_POST['name'];
